dcab23bf2316fd2e5b20ce95d3689c0acf63d4a2
DaniilTS <48275635+DaniilTS@users.noreply.github.com>
2020-09-27 20:55:05 +0300

    Completed UI.

--- Lab1/app/src/main/java/com/example/lab1/KeyboardFragment.java
+++ Lab1/app/src/main/java/com/example/lab1/KeyboardFragment.java
@@ -14,5 +14,6 @@ public class KeyboardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_keyboard, container, false);
+
     }
 }
